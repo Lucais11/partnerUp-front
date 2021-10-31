9978f9ac6222fa8fe00506a184f0994822b1497d
Kaelyn Hyson <kaelynhyson@yahoo.com>
2021-10-31 16:37:15 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+
+const http = async (path, method, payload) => {
+  const serverUrl = "http://localhost:5000";
+  const options = {
+    method,
+  };
+  if (payload) {
+    options.body = JSON.stringify(payload);
+    options.headers = { "Content-Type": "application/json" };
+  }
+  const res = await window.fetch(`${serverUrl}/${path}`, options);
+  const data = await res.json();
+  console.log(data);
+};
+
+const App = () => {
+  useEffect(() => {
+    http("mac", "POST", {
+      kind: "laptop",
+      price: 1200,
+      color: "black",
+    });
+  });
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
